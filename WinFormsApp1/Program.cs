836487f4a8edfd5cefb738283b693c66e49e613d
fbudic21 <fbudic21@foi.hr>
2026-01-29 18:02:16 +0100

    sdf

--- WinFormsApp1/Program.cs
+++ WinFormsApp1/Program.cs
@@ -11,7 +11,6 @@ namespace WinFormsApp1
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            //quac
             Application.Run(new Form1());
         }
     }
