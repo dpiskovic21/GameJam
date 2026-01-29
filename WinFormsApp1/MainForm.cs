da090fb30f4f58ec87912148c2dfc5a73e2a4f91
fbudic21 <fbudic21@foi.hr>
2026-01-29 19:01:17 +0100

    add models and initialize deck

--- WinFormsApp1/MainForm.cs
+++ WinFormsApp1/MainForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WinFormsApp1.models;
 
 namespace WinFormsApp1
 {
@@ -15,6 +16,32 @@ namespace WinFormsApp1
         public MainForm()
         {
             InitializeComponent();
+            this.InitializeDeck();
+        }
+
+        private void InitializeDeck()
+        {
+            List<Card> initDeck = new List<Card>();
+
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = -5; j < 6; j++)
+                {
+                    if (j == 0)
+                    {
+                        continue;
+                    }
+                    Card card = new Card()
+                    {
+                        Value = j,
+                        CardBackImagePath = "shit"
+                    };
+
+                    initDeck.Add(card);
+                }
+            }
+
+            Deck.Cards = initDeck;
         }
     }
 }
---
+++ WinFormsApp1/models/Deck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1.models
+{
+    public static class Deck
+    {
+        public static List<Card> Cards { get; set; } = new List<Card>(60);
+    }
+}
