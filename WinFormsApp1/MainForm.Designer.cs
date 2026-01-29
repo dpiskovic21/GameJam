80023fe14404609b8aee7d44c4d4fdf4f82eecdf
dpiskovic21 <dpiskovic21@foi.hr>
2026-01-29 18:07:25 +0100

    fullsceen opet

--- WinFormsApp1/MainForm.Designer.cs
+++ WinFormsApp1/MainForm.Designer.cs
@@ -28,14 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(263, 911);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ControlDarkDark;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1902, 1033);
+            Controls.Add(button1);
             Name = "MainForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "MainForm";
@@ -44,5 +55,7 @@
         }
 
         #endregion
+
+        private Button button1;
     }
 }
