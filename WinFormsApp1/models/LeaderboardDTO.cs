b1f4ac0616b47c6aeec9ef5fdae719de2219d4e2
fbudic21 <fbudic21@foi.hr>
2026-01-30 21:48:26 +0100

    add communication with the leaderboard spreadsheet

---
+++ WinFormsApp1/models/LeaderboardDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1.models
+{
+    public class LeaderboardDTO
+    {
+        public string username {  get; set; }
+        public int score { get; set; }
+        public DateTime timestamp { get; set; }
+    }
+}
