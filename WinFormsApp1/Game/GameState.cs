77d342633bf4784364b618762337dc5a7aaa8323
fbudic21 <fbudic21@foi.hr>
2026-01-30 11:12:19 +0100

    add constants and other atributes

--- WinFormsApp1/Game/GameState.cs
+++ WinFormsApp1/Game/GameState.cs
@@ -1,26 +1,31 @@
-﻿namespace WinFormsApp1.Game
+﻿using WinFormsApp1.models;
+
+namespace WinFormsApp1.Game
 {
     public static class GameState
     {
         #region global info (immutable)
 
         public const int EnergyAvailableEachTurn = 5;
+        public const int MaxAltarSize = 3;
+        public const int MaxDays = 7;
 
         #endregion
 
         #region Global game state
 
-        public static int Day = 1; //1-7
-        public static int Score = 0;
-        public static int Balance = 0; //light dark balance (-10,10)?
+        public static int Day { get; private set; } = 1;
+        public static int TotalScore { get; private set; } = 0;
+        public static int CurrentBallance { get; private set; } = 0;
+        public static bool IsGameOver { get; private set; } = false;
 
         #endregion
 
 
         #region Turn based props
 
-        public static dynamic Hand { get; set; } //TODO ovo je 5 karti u ruci
-        public static dynamic Altar { get; set; } //TODO ovo je 3 karti na altaru ili kaj vec
+        public static List<Card> Hand { get; set; } = new List<Card>();
+        public static List<Card> Altar { get; set; } = new List<Card>();
         public static int AvailableEnergy { get; set; }
         #endregion
 
